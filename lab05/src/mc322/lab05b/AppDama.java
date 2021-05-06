efb3b15f26f181f0d7e34c275e8dce3de36e3110
Fernando Filho <56443820+FerFilho23@users.noreply.github.com>
2021-05-06 12:11:06 -0300

    Update AppDama.java

--- lab05/src/mc322/lab05b/AppDama.java
+++ lab05/src/mc322/lab05b/AppDama.java
@@ -2,21 +2,19 @@ package mc322.lab05b;
 import java.io.IOException;
 
 public class AppDama {
-    static String[] executaJogo(String caminho) {
+    static String[] executaJogo(String entrada, String saida) {
         CSVHandling csv = new CSVHandling();
         Tabuleiro jogo = new Tabuleiro();
         Tabuleiro vetor = new Tabuleiro();
 
-        csv.setDataSource(caminho);
+        csv.setDataSource(entrada);
         String[] jogadas = csv.requestCommands();
         jogo.imprimirTabuleiro(jogadas);
-        
+        jogo.exportarArquivo(saida);
         return vetor.saidaVetor(jogadas);
     }
     public static void main(String[] args)  throws IOException
     {
-        String[] jogadas = AppDama.executaJogo(args[0]);
-        Tabuleiro jogo = new Tabuleiro();
-        jogo.exportarArquivo(args[1]);
+        String[] jogadas = AppDama.executaJogo(args[0], args[1]);
     }
 }
