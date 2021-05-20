781c00c6db794e710332577b3411156b8a50e009
Fernando dos Reis Santos Filho <fernando.reis@kaffa.com.br>
2021-05-19 22:33:45 -0300

    Upgrade Controle: imlementar construtor, função setScore() e implementação inicial de getAction()

--- lab06/src/mc322/lab06/Controle.java
+++ lab06/src/mc322/lab06/Controle.java
@@ -1,17 +1,89 @@
 package mc322.lab06;
 
+import java.util.Scanner;
+
 public class Controle {
-    Player jogador;
+    private Player jogador;
+    private String statusJogo;
+    private final Scanner keyboard = new Scanner(System.in);
 
-    public void setPlayer(Player hero) { jogador = hero;}
 
-    public void setName()
-    {
+    public Controle(Player player){
+        setPlayer(player);
+        setStatusJogo("Em andamento");
+        System.out.printf("Bem-vindo %s ao mundo de Wumpus!!%n", this.jogador.getName());   //TODO: *Opcional* Escrever a apresentação do jogo e das regras para o jogador
+    }
+
+    public void setStatusJogo(String status) {
+       switch (status){
+           case "Jogador Saiu do Jogo":
+               System.out.println("Volte Sempre!");
+               break;
+           case "Voce perdeu =( ...":
+               System.out.println(status);
+               break;
+           case "Voce ganhou =D !!!":
+               System.out.println(status);
+               break;
+           default:
+       }
+
+       statusJogo = status;
+    }
+
+    public boolean getStatusJogo() {
+        return statusJogo.equals("Em andamento");
+    }
+
+    public void setPlayer(Player hero) {
+        jogador = hero;
+        jogador.setScore(0);
         jogador.setName();
     }
-    public String getName()
-    {
-        return  jogador.getName();
+
+    public void setScore(int pontos){
+        jogador.setScore(pontos);
     }
 
+    public void printPlayer(){
+        System.out.println("Player: " + jogador.getName());
+        System.out.println("Score: " + jogador.getScore());
+        System.out.printf("%nAção:%n");
+    }
+
+    public void getAction(){
+        String action = keyboard.nextLine();
+
+        switch (action){
+            case "w":
+//               jogador.moverCima();
+                setScore(-15);
+                break;
+            case "s":
+//                jogador.moverBaixo();
+                setScore(-15);
+                break;
+            case "d":
+//                jogador.moverDireita();
+                setScore(-15);
+                break;
+            case "a":
+//                jogador.moverEsquerda();
+                setScore(-15);
+                break;
+            case "k":
+//                jogador.equipaFlecha();
+                setScore(-100);
+                break;
+            case "c":
+//                jogador.coletaOuro();
+                break;
+            case "q":
+                setStatusJogo("Jogador Saiu do Jogo");
+                break;
+            default:
+                System.out.println("Ação inválida!! Tente novamente.");
+                getAction();
+        }
+    }
 }
