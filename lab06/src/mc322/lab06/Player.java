9e3f7ec799f53e3ed3fc1cf4b101583e8b34f8f0
Fernando dos Reis Santos Filho <fernando.reis@kaffa.com.br>
2021-05-19 22:35:21 -0300

    Upgrade Player: incluir parâmetros e funções para contar os pontos

--- lab06/src/mc322/lab06/Player.java
+++ lab06/src/mc322/lab06/Player.java
@@ -3,11 +3,22 @@ import java.util.Scanner;
 
 public class Player extends Component {
     private String name;
+    private int score;
     private Scanner keyboard = new Scanner(System.in);
+
     public void setName()
     {
+        System.out.print("Escreva o nome do seu jogador: ");
         name = keyboard.nextLine();
     }
-    public String getName() {return name;}
+    public String getName() {
+        return name;
+    }
 
+    public void setScore(int pontos) {
+        score += pontos;
+    }
+    public int getScore() {
+        return score;
+    }
 }
